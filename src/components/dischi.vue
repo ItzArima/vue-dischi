c3728cfcfb3ee8dae78f166bcb1dd21193560976
ItzArima <skaypaprilia@gmail.com>
2021-11-15 19:48:28 +0100

    loader impostato come componente e impostato timeout iniziale

--- src/components/dischi.vue
+++ src/components/dischi.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="disks-container">
-        <div class="elements" v-if="loading != true">
+        <div class="elements" v-if="this.loading != true">
             <div class="disk-container" v-for="element in elements" :key="element.name">
                 <img :src="element.poster" alt="">
                 <h2>{{element.title}}</h2>
@@ -11,18 +11,21 @@
             </div>
         </div>    
         <div v-else class="loading" >
-            <div class="loading-container">
-                <img src="../assets/img/loading.gif" alt="">
-            </div>
+            <loader />
         </div>
     </div>    
 </template>
 
 <script>
+import loader from './loader'
 import axios from 'axios'
 
 export default {
     name :"dischi",
+
+    components:{
+        loader,
+    },
 
     data(){
         return{
@@ -30,17 +33,27 @@ export default {
             loading: true,
         }
     },
-    mounted(){
-        axios
+    methods :{
+        launch(){
+            axios
             .get('https://flynn.boolean.careers/exercises/api/array/music')
             .then(r => {
                 console.log(r.data)
                 this.elements = r.data.response;
-                this.loading = false 
+                this.loading = false     
             })
             .catch(e =>{
                 console.log(e);
-            })
+            });
+        }
+    },
+
+
+    mounted(){
+
+        setTimeout(this.launch,2000)
+        
+        
     }
 }
 </script>
---
+++ src/components/loader.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="loading-container">
+        <img src="../assets/img/loading.gif" alt="">
+    </div>
+</template>
+
+<script>
+export default {
+    name : "loader"
+}
+</script>
+
+<style>
+
+</style>
